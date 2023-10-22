a5c7f0f56e2e99284b17b325395090b0a174348b
Falk Hofmann <Falk@kombinat-13b.de>
2023-10-22 20:21:14 +0200

    add mandelbrot and renamed tools

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import nuke
 
-GENERATORS = ["lines", "radialGradient", "radialRays", "ringing"]
+GENERATORS = ["Lines", "Mandelbrot", "RadialGradient", "RadialRays", "Ringing"]
 MODIFIERS = ["Haze", "IterationRolloff", "Kontrast", "Vibrant"]
 
 
@@ -8,9 +8,8 @@ def add_nodes_to_menu():
     for menu in ["Nuke", "Nodes"]:
         nuke_menu = nuke.menu(menu)
         kombinat = nuke_menu.addMenu("nuke_kombinat")
-        kombinat_nodes = kombinat.addMenu("Nodes")
-        kombinat_generators = kombinat_nodes.addMenu("generators")
-        kombinat_modifiers = kombinat_nodes.addMenu("MODIFIERS")
+        kombinat_generators = kombinat.addMenu("Generators")
+        kombinat_modifiers = kombinat.addMenu("Modifiers")
 
         for node in GENERATORS:
             kombinat_generators.addCommand(node, "nuke.createNode('{}')".format(node))
