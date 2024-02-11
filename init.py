0e5ad7c58fccd856cc48a4a2f4b19509e2250929
Falk Hofmann <Falk@kombinat-13b.de>
2024-02-11 14:47:17 +0100

    renamed folders

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ import nuke
 
 
 def extend_plugin_path():
-    paths = ["./icons", "./gizmos", "./kombinat_start_up"]
+    paths = ["./kombinat_icons", "./kombinat_gizmos", "./kombinat_start_up"]
 
     for path in paths:
         nuke.pluginAddPath(path)
