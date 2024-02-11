9b93ad7175c0ac98448f8c13bcd1a580649bded9
Falk Hofmann <Falk@kombinat-13b.de>
2024-02-11 11:12:38 +0100

    added pause viewer context manager and viewer utils

--- kombinat_utils/context_manager.py
+++ kombinat_utils/context_manager.py
@@ -4,6 +4,8 @@ from time import perf_counter
 import nuke
 import nukescripts
 
+from kombinat_utils import viewer_utils
+
 
 class Timer:
     "Context manager to measure performance during developing."
@@ -27,6 +29,25 @@ class SaveSelection:
     def __enter__(self) -> None:
         nukescripts.clear_selection_recursive()
 
-    def __exit__(self, exc_type, ec_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
         for node in self.selected_nodes:
             node.setSelected(True)
+
+
+class PausedViewer:
+    """Context manager to pause and unpause the active viewer.
+
+    In case the viewer is paused already the toggle is skipped."""
+
+    def __init__(self) -> None:
+        self.viewer = viewer_utils.find_active_viewer_widget()
+        self.pause_button = viewer_utils.find_view_menu_object(self.viewer, "Pause")
+        self.initial_paused = self.pause_button.isChecked()
+
+    def __enter__(self) -> None:
+        if not self.initial_paused:
+            self.pause_button.toggle()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if not self.initial_paused:
+            self.pause_button.toggle()
