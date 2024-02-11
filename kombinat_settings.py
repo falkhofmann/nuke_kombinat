bbe985c2fd89d748a4d817c928673ab4a5de1cca
Falk Hofmann <Falk@kombinat-13b.de>
2024-02-11 12:54:43 +0100

    add node utils and shortcut menus

---
+++ kombinat_settings.py
@@ -0,0 +1,10 @@
+MENU_NAME = "nuke_kombinat"
+
+GENERATORS = ["Lines", "Mandelbrot", "RadialGradient", "RadialRays", "Ringing"]
+MODIFIERS = ["Haze", "IterationRolloff", "Kontrast", "Vibrant"]
+
+CUSTOM_NODES_SHORTCUTS = {
+    "Color/ColorCorrect": "",
+    "Color/Math/Multiply": "c",
+    "Image/Constant": "n",
+}
